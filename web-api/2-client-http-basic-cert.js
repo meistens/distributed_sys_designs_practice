4a159503f156e099ad4ddc9399e1686d446673a1
David Mebo <mebodave@gmail.com>
2022-12-17 15:52:37 +0000

    certificates, server comms and some trouble using es6

---
+++ web-api/2-client-http-basic-cert.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+import express from 'express';
+import fetch from 'node-fetch';
+import https from 'https';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const HOST = '127.0.0.1';
+const PORT = process.env.PORT || 3000;
+const TARGET = process.env.TARGET || 'localhost:4000';
+const server = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.join(__filename);
+let caPath = path.join(__dirname + '/../shared/tls/basic-certificate.cert');
+
+const options = {
+  agent: new https.Agent({
+    ca: fs.readFileSync(caPath, {encoding: 'utf8'})
+  }),
+};
+
+server.get('/', async () => {
+  const req = await fetch(`https://${TARGET}/recipes/42`, options);
+  const payload = await req.json();
+
+  return {
+    consumer_pid: process.pid,
+    producer_data: payload,
+  };
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`consumer running at ${HOST}:${PORT}`);
+});
